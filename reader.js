014f931599453a5ae38e7d980c7ff0646922ce8a
Angelica Michaca <angelica.michacaco@gmail.com>
2023-03-02 16:42:54 -0500

    Add  js, json, and css files via upload

---
+++ reader.js
@@ -0,0 +1,15 @@
+console.log("Accessibility Rocks!");
+
+$(document).ready(function () { //CODE GOES HERE
+
+  $("*:not(body)").hover( function (ev) {
+    //EXECUTED WHEN MOUSE ENTERS AN ELEMENT
+
+    function (ev) {
+      $(#elementselected).addClass("highlight")
+      $(#elementselected).removeClass("highlight")
+      }
+
+    }
+
+}
